d7982907bf18d78df29e4e23cc6a2741d9c9d603
caconeves <82548404+caconeves@users.noreply.github.com>
2021-05-19 21:16:50 -0300

    atualização de routers

--- controllers/institucionalController.js
+++ controllers/institucionalController.js
@@ -1,6 +1,18 @@
 const institucionalController = {
     index: (request, response) => {
         return response.render('index');
+    },
+    sobre: (request, response) => {
+        return response.render('sobre');
+    },
+    servicos: (request, response) => {
+        return response.render('servicos');
+    },
+    cadastro: (request, response) => {
+        return response.render('cadastro');
+    },
+    contato: (request, response) => {
+        return response.render('contato');
     }
 }
 
--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,19 @@ const petsController = require('../controllers/petsController');
 const institucionalController = require('../controllers/institucionalController');
 
 //rotas para paginas institucionais
+
+// http://localhost:3000/
 router.get('/', institucionalController.index);
+
+// http://localhost:3000/sobre
+router.get('/sobre', institucionalController.sobre);
+
+// http://localhost:3000/contato
+router.get('/contato', institucionalController.contato);
+
+// http://localhost:3000/cadastro
+router.get('/cadastro', institucionalController.cadastro);
+
 
 // rota para /pets que retorna o método index da petsController
 router.get('/pets', petsController.index);
